6fee6b2d549593ef0a51bb34a6ac5974f3415dcd
VaheM27 <vmnatsakanyan27@gmail.com>
2022-08-14 23:32:03 +0400

    commit

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -9,31 +9,10 @@ import { v4 as uuidv4 } from 'uuid';
   styleUrls: ['./cards.component.scss'],
 })
 export class CardsComponent implements OnInit {
-  peopleContacts: any = [
+  block = [
     {
-      id: uuidv4(),
-      email: 'at.velit@hotmail.com',
-      phone: '(383) 960-7566',
-    },
-    {
-      id: uuidv4(),
-      email: 'donec@google.net',
-      phone: '(245) 430-7060',
-    },
-    {
-      id: uuidv4(),
-      email: 'maecenas.iaculis@yahoo.couk',
-      phone: '1-343-279-5772',
-    },
-    {
-      id: uuidv4(),
-      email: 'amet.nulla@google.edu',
-      phone: '(621) 538-5609',
-    },
-    {
-      id: uuidv4(),
-      email: 'nunc.ac.mattis@aol.org',
-      phone: '1-723-358-4146',
+      id: 0,
+      num: 0,
     },
   ];
 
@@ -42,40 +21,23 @@ export class CardsComponent implements OnInit {
   ngOnInit(): void {}
 
   delete(id: any) {
-    this.peopleContacts = this.peopleContacts.filter(
-      (item: any) => item.id !== id
-    );
+    this.block = this.block.filter((item: any) => item.id !== id);
   }
 
-  updateItem(item: any){
-
-    let index = this.peopleContacts.indexOf(item);
-    item.name = "Change Hardik";
-    this.peopleContacts[index] = item;
-
-    console.log(this.peopleContacts);
-  }
-
-
-  openDeleteModal(id: any) {
-    const modalRef = this.modalService.open(DeleteModalComponent, {
-      size: 'lg',
-    });
-    modalRef.componentInstance.data = this.peopleContacts.find(
-      (el: any) => el.id === id
-    );
-    modalRef.componentInstance.delete.subscribe((id: any) => {
-      this.delete(id);
+  setBlock() {
+    const num = Math.ceil(Math.random() * 100000);
+    const id = Math.ceil(Math.random() * 10);
+    this.block.push({
+      id: num,
+      num: id,
     });
   }
 
-  openEditModal(id: any) {
-    const modalRef = this.modalService.open(EditModalComponent, { size: 'lg' });
-    modalRef.componentInstance.peopleContacts = this.peopleContacts.find(
-      (el: any) => el.id === id
-    );
-    modalRef.componentInstance.updateItem.subscribe((id: any) => {
-      this.updateItem(id);
-    });
-  }
+  sortCard() {
+    [
+      ...this.block.sort((a, b) => {
+        return a.num - b.num;
+      }),
+    ];
+  };
 }
