dba3a26df02253b6d6df2a42bd2e4b9cd0a493ae
VaheM27 <vmnatsakanyan27@gmail.com>
2022-08-15 00:03:07 +0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { CardsComponent } from './components/cards/cards.component';
 import { EditModalComponent } from './components/edit-modal/edit-modal.component';
 import { TableComponent } from './components/table/table.component';
 
 @NgModule({
-  declarations: [AppComponent, CardsComponent, TableComponent, EditModalComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    CardsComponent,
+    TableComponent,
+    EditModalComponent,
+  ],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
